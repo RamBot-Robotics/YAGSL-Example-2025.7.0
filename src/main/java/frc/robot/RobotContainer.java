d317bb00b8ff494d24fd1503f7b02fc2b35f1803
RamBot-Robotics <rambotrobotics@gmail.com>
2025-03-04 12:56:29 -0500

    add comment for later

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -32,7 +32,7 @@ public class RobotContainer
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   final         CommandXboxController driverXbox = new CommandXboxController(0);
-  // The robot's subsystems and commands are defined here...
+  // The robot's subsystems and commands are defined here... //TODO use this for test thing.
   private final SwerveSubsystem       drivebase  = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
                                                                                 "swerve/neo"));
 
