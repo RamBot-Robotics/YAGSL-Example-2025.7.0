912a2a272db709ad5e2d771535c5d23ef84af1c6
nonokirby <rambotrobotics@gmail.com>
2025-03-21 00:51:44 -0400

    Fix Warning

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,17 +1,12 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.CANcoder;
-import com.ctre.phoenix6.sim.CANcoderSimState;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.servohub.ServoHub.ResetMode;
 import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj.simulation.EncoderSim;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.climber;
@@ -42,7 +37,7 @@ public class Climber extends SubsystemBase{
 
     public void goSet(double setpoint){
       //sets modEncoderX to be a number where 0 is the bottom of the climber's state and 1 is the top
-      double modEncoderL = mclimber.getEncoder().getPosition()/climber.ConversionRate; 
+    //   double modEncoderL = mclimber.getEncoder().getPosition()/climber.ConversionRate; 
       mclimber.set(kPID.calculate(mclimber.getEncoder().getPosition(), setpoint));
       SmartDashboard.putNumber("Climber/climber/Setpoint", setpoint);
       SmartDashboard.putNumber("Climber/climber/Setpoint", setpoint);
